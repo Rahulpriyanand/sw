bc25dd99c377da52bdb05ef36332145519b5ef18
Rahulpriyanand <47180230+Rahulpriyanand@users.noreply.github.com>
2021-02-21 00:01:10 +0530

    Update push.js

--- push.js
+++ push.js
@@ -3,9 +3,9 @@ var push = require('web-push');
 let vapidkeys = {
     publicKey: 'BDc7gIaJjKSJBuQEwgXaYOZx_T9PCFODtW0AX0wzKSUu-Bl7543LFjjYZRE0D7pHXxZ48wFdXSzYzLv6IocniLk',
     privateKey: 'dTO2T9s-y6vnOh7U3Yb5c-Z0KXOt5RKHMZ1yP0KDpic'
-  }
+  } 
 push.setVapidDetails('mailto:rahulshakyaame@gmail.com' vapidkeys.publicKey, vapidkeys.privateKey)
 
 let sub = {};
 
-  push.sendNotification(sub, 'test message ' ) 
+  push.sendNotification(sub, 'test message ')
