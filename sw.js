d4f7ee558ce95d3b04ad19e105b1c384a3938e61
Rahulpriyanand <47180230+Rahulpriyanand@users.noreply.github.com>
2021-02-21 09:20:29 +0530

    sa

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
 self.addEventListener('push', function(e) {
     var options = {
       body: 'This notification was generated from a push!',
-      icon: 'images/example.png',
+      icon: 'https://www.amecet.in/index.php',
       vibrate: [100, 50, 100],
       data: {
         dateOfArrival: Date.now(),
         primaryKey: '2'
       },
       actions: [
-        {action: 'explore', title: 'Explore this new world',
-          icon: 'images/checkmark.png'},
+        {action: 'explore', title: 'Hai, welcone to subscribe us hang On.',
+          icon: 'https://www.amecet.in/index.php'},
         {action: 'close', title: 'Close',
           icon: 'images/xmark.png'},
       ]
